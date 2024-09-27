f2f4527c33b58c973e5355dd63dfa958111f7c28
riteshlakhani25 <ritesh.lakhani@gmail.com>
2024-09-26 20:40:06 -0400

    adding app and main jsx

--- RiteshLakhani_WebDevelopment_COMP229_Assignment1_Portfolio/src/App.jsx
+++ RiteshLakhani_WebDevelopment_COMP229_Assignment1_Portfolio/src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+//import { Link } from 'react-router-dom';
+/*export default function Layout() {
+ return (
+ <>
 
-function App() {
-  const [count, setCount] = useState(0)
 
+ <h1>My Portfolio</h1>
+ 
+ <nav>
+    
+ <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/education">Education</Link>| <Link to="/project">Project</Link>| <Link to="/contact">Contact</Link>
+ </nav>
+<br/>
+
+
+TEST
+ <hr />
+ 
+ </>
+ 
+ );
+}
+*/
+//import { formatDiagnosticsWithColorAndContext } from "typescript";
+
+function RiteshComponent() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count} 
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  
+  <a href="https://www.linkedin.com/in/ritesh-lakhani-pm-sm/">My Portfolio</a>
+   );
 }
 
-export default App
+export default function MyApp() {
+  return (
+    <>
+      <h1>Ritesh Lakhani</h1>
+      
+      <RiteshComponent />
+      
+    
+    <a href="/">Home</a> | <a href="/about">About</a> | <a href="/education">Education</a>| <a href="/project">Project</a>| <a href="/contact">Contact</a>
+    
+    </>
+          
+    
+  );
+}
+
