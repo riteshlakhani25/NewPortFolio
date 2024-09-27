719d38b0f64e9a9cfd59fe31c4e2043975d938f9
riteshlakhani25 <ritesh.lakhani@gmail.com>
2024-09-26 20:52:52 -0400

    adding other pages

--- RiteshLakhani_WebDevelopment_COMP229_Assignment1_Portfolio/src/App.jsx
+++ RiteshLakhani_WebDevelopment_COMP229_Assignment1_Portfolio/src/App.jsx
@@ -1,28 +1,4 @@
 import React from 'react';
-//import { Link } from 'react-router-dom';
-/*export default function Layout() {
- return (
- <>
-
-
- <h1>My Portfolio</h1>
- 
- <nav>
-    
- <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/education">Education</Link>| <Link to="/project">Project</Link>| <Link to="/contact">Contact</Link>
- </nav>
-<br/>
-
-
-TEST
- <hr />
- 
- </>
- 
- );
-}
-*/
-//import { formatDiagnosticsWithColorAndContext } from "typescript";
 
 function RiteshComponent() {
   return (
@@ -35,11 +11,12 @@ export default function MyApp() {
   return (
     <>
       <h1>Ritesh Lakhani</h1>
+      <tex><RiteshComponent /> <br /></tex>
       
-      <RiteshComponent />
+      
       
     
-    <a href="/">Home</a> | <a href="/about">About</a> | <a href="/education">Education</a>| <a href="/project">Project</a>| <a href="/contact">Contact</a>
+    <a href="/">Home</a> | <a href="./about">About</a> | <a href="./education">Education</a>| <a href="./project">Project</a>| <a href="./contact">Contact</a>
     
     </>
           
